88f74dd758dcaefe2bd60daad8beb9c667475c93
Roberto Narvaez <roberto.narvaez@udla.edu.ec>
2024-01-24 16:20:59 -0500

    Final de codigo

--- copro.c
+++ copro.c
@@ -22,11 +22,15 @@ int main(int argc, char const *argv[])
     switch (opcion)
     {
     case 1:
+
         int numerodeproductos;
         printf("Ingrese el numero de productos que va a ingresar : ");
         scanf("%d", &numerodeproductos);
         for (int i = 0; i < numerodeproductos; i++)
         {
+            /*Implementacion de archivos*/
+            FILE *archivo;
+            archivo = fopen("datosproductos.txt", "a");
             printf("\n");
             printf("Ingrese codigo del producto: ");
             scanf("%s", datosproductos.codigo);
@@ -37,12 +41,34 @@ int main(int argc, char const *argv[])
             printf("Ingrese cantidad del producto: ");
             scanf("%d", &datosproductos.cantidadproducto);
             printf("Ingrese precio del producto: ");
-            scanf("%d", &datosproductos.preciodecompra);
+            scanf("%f", &datosproductos.preciodecompra);
+            fprintf(archivo, " %s, %s, %s, %d, %f", datosproductos.codigo, datosproductos.nombreproducto, datosproductos.descripcion, datosproductos.cantidadproducto, datosproductos.preciodecompra);
+            fclose(archivo);
         }
 
         break;
     case 2:
-        /* code */
+        /*Implementacion de archivos para leer lo que este dentro */
+        FILE *archivo;
+        archivo = fopen("datosproductos.txt", "r");
+        if (archivo == NULL)
+        {
+            printf("Error al abrir el archivo.\n");
+            return 1;
+        }
+
+        printf("\n");
+        printf("Codigo,  Nombre, Descripcion, Cantidad, Precio\n");
+        while (fscanf(archivo, "%s %s %s %d %f ", datosproductos.codigo, datosproductos.nombreproducto, datosproductos.descripcion, &datosproductos.cantidadproducto, &datosproductos.preciodecompra) != EOF)
+        {
+            printf("\n");
+            /* \t sirve para tabular de forma horizontal y no vertical como el \n */
+            printf("%s \t %s\t  %s\t  %d\t  %f \n", datosproductos.codigo, datosproductos.nombreproducto, datosproductos.descripcion, datosproductos.cantidadproducto, datosproductos.preciodecompra);
+        }
+
+        fclose(archivo);
+        break;
+
         break;
     case 3:
         printf("Saliendo de tienda UPLO...\n");
